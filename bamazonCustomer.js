a6fdcf7ac046e0fe94fb6046c8272815d5ccac19
Montebo <28013382+mbmedhurst@users.noreply.github.com>
2019-04-28 20:54:40 -0700

    successful connection to db

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,16 @@
 const inquirer = require('inquirer')
-const mysql = required('mysql2')
+const {createConnection} = require('mysql2')
+
+const db = createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root1234',
+    database: 'bamazon_db'
+})
+
+db.connect(e => {
+    if(e) {console.log(e)
+    } else {
+        console.log('Successful connection...')
+    } 
+})
