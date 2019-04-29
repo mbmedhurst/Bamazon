7d915ec00c07df16bb4d4af4df3122c314b2faa3
Montebo <28013382+mbmedhurst@users.noreply.github.com>
2019-04-29 07:49:02 -0700

    processOrder query is working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,5 @@
 const inquirer = require('inquirer')
-const {createConnection} = require('mysql2')
+const { createConnection } = require('mysql2')
 
 const db = createConnection({
     host: 'localhost',
@@ -9,8 +9,55 @@ const db = createConnection({
 })
 
 db.connect(e => {
-    if(e) {console.log(e)
+    if (e) {
+        console.log(e)
     } else {
-        console.log('Successful connection...')
-    } 
+        db.query('SELECT * FROM products', (e, data) => {
+            if (e) {
+                console.log(e)
+            } else {
+                data.forEach(({ item_id, product_name, department_name, price, stock_quantity }) => console.log(`
+                ID: ${item_id}
+                Product: ${product_name}
+                Department: ${department_name}
+                Price: $${price}
+                In Stock: ${stock_quantity}
+                `))
+                takeOrder()
+            }
+        })
+    }
 })
+
+let takeOrder = _ => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'askID',
+            message: 'Please enter the ID number of the product you would like to buy?',
+        },
+        {
+            type: 'input',
+            name: 'howMany',
+            message: 'How many units of this product would you like?'
+        }
+    ])
+        .then(({askID, howMany}) => {
+            console.log(askID)
+            console.log(howMany)
+            processOrder(askID, howMany)
+
+        })
+        .catch(e => console.log(e))
+}
+
+let processOrder = (askID, howMany) => {
+    db.query(`SELECT stock_quantity FROM products WHERE item_id = ${askID}`, (e, r) => {
+        if (e) {
+            console.log(e)
+        } else if ("stock_quantity" >= howMany) {
+            console.log('yes')
+        }
+    })
+}
+            
