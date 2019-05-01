8d31dda5c6d431f64c232503d08ff14167c7f4d5
Montebo <28013382+mbmedhurst@users.noreply.github.com>
2019-04-30 22:02:41 -0700

    view products working

--- bamazonManager.js
+++ bamazonManager.js
@@ -6,3 +6,62 @@ const db = createConnection({
     user: 'root',
     password: 'root1234',
     database: 'bamazon_db'
+})
+
+// displays the entire product list
+let viewProducts = _ => {
+    db.query('SELECT * FROM products', (e, data) => {
+        if (e) {
+            console.log(e)
+        } else {
+            data.forEach(({ item_id, product_name, department_name, price, stock_quantity }) => console.log(`
+                **********************************
+                ID: ${item_id}
+                Product: ${product_name}
+                Department: ${department_name}
+                Price: $${price}
+                In Stock: ${stock_quantity}
+                **********************************
+            `))
+        }
+    })
+}
+
+
+let mgrMenu = _ => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'menu',
+            message: 'What would you like to do?',
+            choices: ['View Products For Sale', 'View Low Inventory', 'Add New Inventory', 'Add New Product', 'Exit App']
+        }
+    ])
+        .then(({ menu }) => {
+            switch (menu) {
+                case 'View Products For Sale':
+                    viewProducts()
+                    break
+                case 'View Low Inventory':
+                    viewInventory()
+                    break
+                case 'Add New Inventory':
+                    addInventory()
+                    break
+                case 'Add New Product':
+                    addProduct()
+                    break
+                case 'Exit App':
+                    process.exit()
+                default:
+                    mgrMenu()
+                    break
+            }
+        })
+        .catch(e => console.log(e))
+}
+
+db.connect(e => {
+    if (e) throw e
+    mgrMenu()
+})
